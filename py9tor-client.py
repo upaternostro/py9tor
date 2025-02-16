029c3f9f59b6b7bfb9a852a9ca309a31cfdda5c0
Ugo Paternostro <ugo.git@paternostro.org>
2025-02-16 17:38:07 +0100

    Adding support to status printing in py9tor-client

--- py9tor-client.py
+++ py9tor-client.py
@@ -2,12 +2,22 @@
 import argparse
 import xmlrpc.client
 from py9tor.configuration import Py9torConfig
+import json
 
 parser = argparse.ArgumentParser(description='Invoke py9tor.')
-parser.add_argument('target', type=str, 
-                    help='the target to invoke')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-t', '--target', help='the target to invoke')
+group.add_argument('-s', '--status', action="store_true", help='print py9tor status')
 args = parser.parse_args()
 
 s = xmlrpc.client.ServerProxy(Py9torConfig().uri(), allow_none=True)
 
-s.start(args.target)
+if (args.status):
+    status = s.status()
+    start = (str)(status['start'])
+    status['start'] = start
+    print(json.dumps(status, indent=2))
+elif (args.target is not None):
+    s.start(args.target)
+else:
+    parser.print_help()
